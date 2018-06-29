4aed7cc51a729b84c4990a441834b97f1d15b9a3
RyuaNerin <admin@ryuanerin.kr>
2018-06-30 01:00:03 +0900

    어머나 반대로 입력했네

--- DaruDaru/Marumaru/ComicInfo/WasabiPage.cs
+++ DaruDaru/Marumaru/ComicInfo/WasabiPage.cs
@@ -28,8 +28,8 @@ namespace DaruDaru.Marumaru.ComicInfo
         }
 
         private ImageInfomation[] m_images;
-        public string FilePath => this.m_cur.SavePath;
-        public string FileDir  => this.ComicNoName != null ? Path.Combine(this.FileDir, ReplaceInvalid(this.ComicNoName) + ".zip") : null;
+        public string FilePath => this.ComicNoName != null ? Path.Combine(this.FileDir, ReplaceInvalid(this.ComicNoName) + ".zip") : null;
+        public string FileDir  => this.m_cur.SavePath;
 
         public string ArchiveCode => GetArchiveCode(this.Url);
 
