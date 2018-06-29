3d3cded83fee1859d4a8e3815649485be5709f09
RyuaNerin <admin@ryuanerin.kr>
2018-06-30 00:19:16 +0900

    보기좋게 수정

--- DaruDaru/Core/Converters/ProgressColorConverter.cs
+++ DaruDaru/Core/Converters/ProgressColorConverter.cs
@@ -19,20 +19,15 @@ namespace DaruDaru.Core.Converters
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var state = (MaruComicState)value;
-            if (state == MaruComicState.Complete_1_Downloaded)
-                return this.BrushCompete;
-
-            if (state == MaruComicState.Error_1_Error)
-                return this.BrushError;
-
-            if (state == MaruComicState.Complete_2_Archived)
-                return this.BrushDownloaded;
-
-            if (state == MaruComicState.Error_2_Protected)
-                return this.BrushProtected;
-
-            return this.BrushNormal;
+            switch ((MaruComicState)value)
+            {
+                case MaruComicState.Complete_1_Downloaded:  return this.BrushCompete;
+                case MaruComicState.Complete_2_Archived:    return this.BrushDownloaded;
+                case MaruComicState.Complete_3_NoNew:       return this.BrushNoNew;
+                case MaruComicState.Error_1_Error:          return this.BrushError;
+                case MaruComicState.Error_2_Protected:      return this.BrushProtected;
+                default:                                    return this.BrushNormal;
+            }
         }
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
