dfe7614639177f684594ea5dc937f3c74e59eb36
RyuaNerin <admin@ryuar.in>
2020-02-22 12:08:19 +0900

    CloudFlareUtilities -> CloudflareSolverRe

--- DaruDaru/Utilities/HttpClientEx.cs
+++ DaruDaru/Utilities/HttpClientEx.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using CloudFlareUtilities;
+using CloudflareSolverRe;
 
 namespace DaruDaru.Utilities
 {
